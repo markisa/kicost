5d3774c051aa7c9362b2dcee7c65e338bb1e8cc4
marko <markisa.jvm@gmail.com>
2016-08-29 10:23:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ test_requirements = [
 
 setup(
     name='kicost',
-    version=kicost.__version__,
+    # version=kicost.__version__,
     #__version__ = '1.0.0',
     description="Build cost spreadsheet for a KiCad project.",
     long_description=readme + '\n\n' + history,
